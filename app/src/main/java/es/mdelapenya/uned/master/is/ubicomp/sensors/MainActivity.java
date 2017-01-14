04aa33ec74dbb7f052637d7a2d64d89233a33fc6
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-01-14 22:05:33 +0100

    Check if the Linear Acceleration Sensor is present

--- app/src/main/java/es/mdelapenya/uned/master/is/ubicomp/sensors/MainActivity.java
+++ app/src/main/java/es/mdelapenya/uned/master/is/ubicomp/sensors/MainActivity.java
@@ -1,13 +1,29 @@
 package es.mdelapenya.uned.master.is.ubicomp.sensors;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null){
+            // Success! There's a linear acceleration sensor.
+        }
+        else {
+            Toast.makeText(
+                MainActivity.this, "No Accelerometer Sensor detected", Toast.LENGTH_SHORT).show();
+        }
 
         setContentView(R.layout.activity_main);
     }
