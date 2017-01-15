85928a086de811ca9f235eb0078a8f6db3456037
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-01-15 10:25:51 +0100

    Add author

--- app/src/main/java/es/mdelapenya/uned/master/is/ubicomp/sensors/MainActivity.java
+++ app/src/main/java/es/mdelapenya/uned/master/is/ubicomp/sensors/MainActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+/**
+ * @author Manuel de la Peña
+ */
 public class MainActivity extends AppCompatActivity {
 
     private SensorManager sensorManager;
--- app/src/test/java/es/mdelapenya/uned/master/is/ubicomp/sensors/ExampleUnitTest.java
+++ app/src/test/java/es/mdelapenya/uned/master/is/ubicomp/sensors/ExampleUnitTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ *
+ * @author Manuel de la Peña
  */
 public class ExampleUnitTest {
 
